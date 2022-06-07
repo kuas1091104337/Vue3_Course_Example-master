d42d663b6838575b983510d47f5d8ed885d2fea5
kuas1091104337 <kuas1091104337@gmail.com>
2022-06-08 01:49:15 +0800

    6/8 setup

--- 01-JS-ES6/example/js/Tools.js
+++
@@ -1,8 +0,0 @@
-const Add = (a, b) => {
-  return a + b;
-};
-
-export const Name = "Mike";
-export const Age = 12;
-
-export default Add;
---
+++ 01-JS-ES6/js/Tools.js
@@ -0,0 +1,5 @@
+const add = (a, b) => a + b;
+export const name = 'mike', age = 12,
+  remove = (a, b) => a - b;
+// default 只能有1個
+export default add;
